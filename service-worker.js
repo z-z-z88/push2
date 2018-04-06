1cbac49a69167763569a6f43e4f7ff8153818abd
z-z-z88 <38129009+z-z-z88@users.noreply.github.com>
2018-04-06 17:05:16 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+self.addEventListener('push', function(event) {
+ const payload = event.data ? event.data.text() : 'no payload';
+ event.waitUntil(self.registration.showNotification('ServiceWorker Cookbook', {body: payload,}));
+});
